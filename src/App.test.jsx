685f9da0b2338a1b6eab60d42b306b628cd54915
peterdyer7 <peter_dyer@hotmail.com>
2018-12-14 12:27:11 -0500

    added testing utils, setup testing, created first test

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import App from './App';
+
+describe('<App />', () => {
+  it('renders and contains 1 <div>', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper).toContainExactlyOneMatchingElement('div');
+  });
+});
