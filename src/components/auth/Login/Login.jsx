4cd9af6a742096a25620f04f32f02c1d80de7d7b
peterdyer7 <peter_dyer@hotmail.com>
2018-12-19 12:20:01 -0500

    replacing Enzyme with react-testing-library

--- src/components/auth/Login/Login.jsx
+++ src/components/auth/Login/Login.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+// import React from 'react';
 
 export default function Login() {
+  useEffect(() => {
+    import('../../user/Properties/Properties');
+  }, []);
   return <div>Login</div>;
 }
