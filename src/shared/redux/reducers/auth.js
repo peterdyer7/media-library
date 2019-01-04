d3d8e238f68c8ac1d6cb415d698ec58f418d25d3
peterdyer7 <peter_dyer@hotmail.com>
2019-01-04 15:18:00 -0500

    added redux (with helpers), and auth actions and reducer

---
+++ src/shared/redux/reducers/auth.js
@@ -0,0 +1,60 @@
+import {
+  AUTH_START,
+  AUTH_SUCCESS,
+  AUTH_FAIL,
+  AUTH_LOGOUT
+} from '../actions/auth';
+
+const INITIAL_STATE = {
+  user: {},
+  error: null,
+  loading: false
+};
+
+const start = (state, action) => ({
+  ...state,
+  error: null,
+  loading: true
+});
+
+const fail = (state, action) => ({
+  ...state,
+  user: {},
+  error: action.error,
+  loading: false
+});
+
+const updateUser = (state, action) => ({
+  ...state,
+  user: action.user,
+  error: null,
+  loading: false
+});
+
+const removeUser = (state, action) => ({
+  ...state,
+  user: {},
+  error: null,
+  loading: false
+});
+
+const auth = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case AUTH_START: {
+      return start(state, action);
+    }
+    case AUTH_SUCCESS: {
+      return updateUser(state, action);
+    }
+    case AUTH_FAIL: {
+      return fail(state, action);
+    }
+    case AUTH_LOGOUT: {
+      return removeUser(state, action);
+    }
+    default:
+      return state;
+  }
+};
+
+export default auth;
