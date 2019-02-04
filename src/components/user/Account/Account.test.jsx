fd2511988cdc66d14a8d3376128716d84e30ed22
peterdyer7 <peter_dyer@hotmail.com>
2019-02-04 12:57:52 -0500

    added testing for ResetPasswordForm

--- src/components/user/Account/Account.test.jsx
+++ src/components/user/Account/Account.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, fireEvent, wait } from 'react-testing-library';
 
 import Account from './Account';
 
@@ -17,5 +17,43 @@ describe('<Account />', () => {
 
     expect(getByText('User Settings')).toBeInTheDocument();
     expect(getByText(email)).toBeInTheDocument();
+  });
+
+  it('renders and shows the ResetPasswordForm when button clicked', async () => {
+    const email = 'auser@example.com';
+    const { getByText, getByTestId } = render(
+      <Account
+        user={{ uid: '123', email: email }}
+        loading={false}
+        error=""
+        boundResetPassword={() => {}}
+      />
+    );
+
+    expect(getByText('User Settings')).toBeInTheDocument();
+    expect(getByText(email)).toBeInTheDocument();
+
+    const showButton = getByTestId('show');
+    expect(showButton).not.toBeDisabled();
+    expect(showButton).toHaveAttribute('type', 'button');
+    fireEvent.click(showButton);
+
+    const submitButton = getByTestId('submit');
+    expect(submitButton).toBeDisabled();
+    expect(submitButton).toHaveAttribute('type', 'submit');
+
+    const newPasswordInput = getByTestId('newPasswordInput');
+    expect(newPasswordInput).toBeInTheDocument();
+    fireEvent.change(newPasswordInput, { target: { value: 'password' } });
+
+    expect(submitButton).not.toBeDisabled();
+
+    await wait(() => {
+      fireEvent.click(submitButton);
+    });
+
+    await wait(() => {
+      expect(newPasswordInput).not.toBeInTheDocument();
+    });
   });
 });
