7db1b158e242ba4252b73ae0eed1b0cd238a3e1a
peterdyer7 <peter_dyer@hotmail.com>
2019-03-08 12:30:54 -0500

    updated Images and Image components

--- src/containers/user/Properties/Property/PropertyContainer.jsx
+++ src/containers/user/Properties/Property/PropertyContainer.jsx
@@ -2,28 +2,16 @@ import { connect } from 'react-redux';
 
 import Property from '../../../../components/user/Properties/Property/Property';
 import { propertyFetch } from '../../../../shared/redux/actions/properties';
-import { imagesPropertyFetch } from '../../../../shared/redux/actions/images';
-import { fetchSettings } from '../../../../shared/redux/actions/settings';
 
 const mapStateToProps = (state, ownProps) => ({
   property: state.properties.properties.filter(
     (property) => property.id === ownProps.match.params.propertyId
   )[0],
-  propertyLoading: state.properties.loading,
-  propertyError: state.properties.error,
-  images: state.images.images.filter(
-    (image) =>
-      image.properties.includes(ownProps.match.params.propertyId) &&
-      image.active
-  ),
-  imagesLoading: state.images.loading,
-  iamgesError: state.images.error,
-  settings: state.settings.settings
+  loading: state.properties.loading,
+  error: state.properties.error
 });
 const mapDispatchToProps = (dispatch) => ({
-  boundPropertyFetch: (id) => dispatch(propertyFetch(id)),
-  boundImagesPropertyFetch: (id) => dispatch(imagesPropertyFetch(id)),
-  boundSettingsFetch: (type) => dispatch(fetchSettings(type))
+  boundPropertyFetch: (id) => dispatch(propertyFetch(id))
 });
 
 export default connect(
